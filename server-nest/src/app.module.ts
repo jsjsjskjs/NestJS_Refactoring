706fca511662e4c20153b1a82bf28d5a683621ca
jsjsjskjs <jsjsjskjs@gmail.com>
2022-02-01 00:40:43 +0900

    controller 세팅

--- server-nest/src/app.module.ts
+++ server-nest/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { GroundsModule } from './grounds/grounds.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PostsModule, GroundsModule],
   controllers: [AppController],
   providers: [AppService],
 })
